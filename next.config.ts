9542af491da790af10767d3145b17cdf396f40a6
Damian Campbell <damiancampbell@Mac.attlocal.net>
2025-08-10 10:38:19 -0400

    adding a base path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/goat-demo",
   output: "export", // <=== enables static exports
   reactStrictMode: true,
 };
