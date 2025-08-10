c70dbc4908b33a4408dabf9297f881575f73971f
Damian Campbell <damiancampbell@Mac.attlocal.net>
2025-08-10 10:20:33 -0400

    updating eslint config file to ignore some minor warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
